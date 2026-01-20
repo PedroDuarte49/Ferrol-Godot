be737b1585f589ab6ac8c02136e17c7368959173
Nicolás Vázquez <nvazquezs24@fpcoruna.afundacion.org>
2026-01-20 09:09:06 +0100

    pantalla GameOver

---
+++ ferrol-el-juego/scenes/game_over.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _on_volver_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn") 
+
+func _on_subir_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn") 
