e09c3b5728dd56b5c9e145abc8bc97ff482289f3
Breixo Gonzalez <bgonzalezp24@fpcoruna.afundacion.org>
2026-01-22 14:18:51 +0100

    creado scoreboard funcional, cambio en botones upload de game over y de win para qeu una vez subida la puntuacion nos lleva al scoreboard en juego

--- ferrol-el-juego/scripts/game_manager.gd
+++ ferrol-el-juego/scripts/game_manager.gd
@@ -8,7 +8,7 @@ var current_level: Node = null
 var current_level_path := ""
 var player_spawn_tag := "Spawn"
 
-var score := 0
+var score := 5000
 var saved_score := 0
 # Called when the node enters the scene tree for the first time.
 
