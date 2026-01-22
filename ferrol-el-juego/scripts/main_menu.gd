1aff88fa97f99fbb68daf39c2c98e7c1915dc353
Breixo Gonzalez <bgonzalezp24@fpcoruna.afundacion.org>
2026-01-22 10:23:57 +0100

    ajustes en menuprincipal y menu opciones, botones funcionan

--- ferrol-el-juego/scripts/main_menu.gd
+++ ferrol-el-juego/scripts/main_menu.gd
@@ -2,13 +2,12 @@ extends Control
 
 func _on_play_pressed():
 	
-	get_tree().change_scene_to_file("res://how_to.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_level.tscn")
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://opciones.tscn")
+	get_tree().change_scene_to_file("res://scenes/opciones.tscn")
 
 func _on_how_to_pressed():
-
 	get_tree().change_scene_to_file("res://how_to.tscn")
 
 func _on_exit_pressed():
