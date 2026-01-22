50e39eee268031ad9ba16d02bc8c1a9e4196df50
Breixo Gonzalez <bgonzalezp24@fpcoruna.afundacion.org>
2026-01-22 17:14:32 +0100

    creado enemigo fuerte y colegiala

---
+++ ferrol-el-juego/scripts/colegiala.gd
@@ -0,0 +1,26 @@
+extends Enemy
+var awakened = false
+
+
+func _ready():
+	super._ready()
+
+	# Stats modificados
+	health = 20
+	speed = 120
+	attack_power = 15
+
+func state_idle(delta):
+	velocity = Vector2.ZERO
+	play_anim("idle")
+
+	# NO persigue al jugador mientras duerme
+	if awakened:
+		state = State.CHASE
+
+func take_damage(amount: int, from_position: Vector2, attack_type: int):
+	if not awakened:
+		awakened = true
+		state = State.CHASE
+	#super hace que el resto de la funcion sea como la de enemy
+	super.take_damage(amount, from_position, attack_type)
